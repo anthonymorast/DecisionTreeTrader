c886255afb557ddceef8f056142b4db2750437e6
Anthony Morast <anthonymorast33@hotmail.com>
2023-12-28 18:06:17 -0700

    completed post

---
+++ AssetAgnosticIndicators.py
@@ -0,0 +1,14 @@
+"""
+    Use interpretable machine learning algorithms again but now
+    use indicators that are not dependant on the underlying asset.
+    For example, use RSI, MACD crossover signals (e.g. is the signal
+    line above or below the blew blew), etc. Predict % return 1mo
+    from now for daily data, 6mo from now using weekly data.
+
+    Creates much more training data.
+
+    Example Indicators
+        - RSI
+        - MACD
+        - Standard Deviation
+"""
